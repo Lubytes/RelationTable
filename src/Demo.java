e30b0d6a0f2a2de5dc93d75f18e012d41148dc0a
Brandon <dood0@live.ca>
2016-01-25 09:22:32 -0400

    Initial Input of Written Code
    Inputting code already written by myself. This will be the baseline and
    should be improved from here.

---
+++ src/Demo.java
@@ -0,0 +1,123 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//Demo class for Assignment 5
+//Brandon Poole
+//Hey Jonathan!! :DD
+public class Demo {
+
+	public static void main (String [] args){
+		
+		//some initialization of things
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Hey! Enter in a file :D ");
+		String file = kb.nextLine();
+		
+		//set the matrix from the file
+		int[][] matrix = setMatrix(file);
+		
+		//read the matrix. Probably should have used tostring
+		for(int i=0; i<matrix.length; i++){
+			for(int j=0; j<matrix.length; j++){
+				System.out.print(matrix[i][j] + " ");
+				
+			}
+			System.out.println();
+		}
+		
+		//traverse(matrix);
+		ArrayList<String> trav = traverse(matrix);
+		System.out.println(trav.toString());
+		
+		
+		
+		
+	}
+	
+	
+	//creates the heap from reading a file
+	public static int[][] setMatrix(String f){
+
+		int[][]	adjMatrix = null;
+		int num = 0;
+		File file = new File(f);
+
+		//gets the first value as it is different from the rest of the file
+		//The first number represents the number of letters there are
+		//TODO If number is not supplied, find it in n time.
+		try{
+			Scanner scan = new Scanner(file);
+			String line = scan.nextLine();
+			StringTokenizer tok = new StringTokenizer(line, "\t");
+			num = Integer.parseInt(tok.nextToken());
+			
+			adjMatrix = new int [num][num];
+
+			//creates the processes and adds them to the heap
+			//the heap is created by looking at the initiation time
+			while(scan.hasNext()){
+				line = scan.nextLine();
+				tok = new StringTokenizer(line, "\t");
+				String a = tok.nextToken();
+				String b = tok.nextToken();
+
+				int	aa	=	a.charAt(0)-65;
+				int	bb	=	b.charAt(0)-65;
+				
+				adjMatrix[aa][bb] = 1;
+				adjMatrix[bb][aa] = 1;
+
+			}
+			scan.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("Oops! No File Error :(");
+		}
+		return adjMatrix;
+	}
+	
+	
+	public static ArrayList<String> traverse(int[][] matrix){
+		
+		ArrayList<String> ret = new ArrayList<String>();
+		Queue<String> trav = new Queue<String>();
+		
+		//enqueue String value of the digit. 
+		//The [0] is needed because toChars returns an array of chars
+		trav.enqueue(String.valueOf(Character.toChars(65)[0]));
+		
+		
+		
+		
+		while(!trav.isEmpty()){
+			
+			//dequeue the next vertex
+			ret.add(trav.dequeue());
+			
+			
+			//find and add neighbors to queue 
+			for(int i=0; i<matrix.length; i++){
+				
+				//if the value in the matrix is a 1, and the value has not been added to the array or queue.
+				if(matrix[ret.get(ret.size()-1).charAt(0)-65][i] == 1 && !ret.contains(String.valueOf(Character.toChars(i+65)[0])) && trav.positionOf(String.valueOf(Character.toChars(i+65)[0])) ==-1){
+					//add it to the queue
+					trav.enqueue(String.valueOf(Character.toChars(i+65)[0]));
+				}
+				
+			}
+			
+			
+		}
+		
+		return ret;
+		
+		
+		
+	}
+	
+	
+}
+
---
+++ src/Queue.java
@@ -0,0 +1,74 @@
+//Queue class that makes a generic queue. 
+//This class works with a linked list to make the queue
+public class Queue<T> {
+
+	private LinkedList<T> list;
+	int cursor;
+
+	public Queue(){
+		list = new LinkedList<T>();
+		cursor = 0;
+	}
+
+	//add an item to the end of the list
+	public void enqueue(T item){
+		list.addToEnd(item);
+	}
+
+	//remove an item from the front of the list
+	public T dequeue(){
+		return list.removeAt(0);
+	}
+	
+	//displays the size of the list
+	public int Getsize(){
+		return list.size();
+	}
+
+	//boolean answer if it is empty
+	public boolean isEmpty(){
+		return list.isEmpty();
+	}
+
+	//empies the list
+	public void clear(){
+		list.clear();
+	}
+
+	//Look at the front node
+	public T peek(){
+		return list.getAt(0);
+	}
+
+	//gives the int position of an item. -1 if none
+	public int positionOf(T item){
+		return list.indexOf(item);
+	}
+
+	//remove a specific item
+	public void remove(T item){
+		list.remove(item);
+	}
+
+	
+	//first and next use a cursor to navigate the queue
+	public T first(){
+		if (list.isEmpty())
+			return null;
+		cursor = 0;
+		return list.getAt(0);
+
+	}
+
+	public T next(){
+		if(cursor<0||cursor == list.size()-1)
+			return null;
+		cursor++;
+		return list.getAt(cursor);
+	}
+
+	//enumerate the queue
+	public void stateItems(){
+		list.enumerate();
+	}
+}
